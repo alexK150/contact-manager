0e600a1c7b84db3df9e503176eb08dfecda413f6
Alex <akuzin20@gmail.com>
2019-10-04 10:44:31 +0300

    init v12 +Contacts Context & Global State

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,21 @@ import Navbar from "./components/layout/Navbar";
 import {Route, Switch} from "react-router-dom";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
+import ContactState from "./context/contact/ContactState";
 
 const App = () => {
     return (
-        <>
-            <Navbar title='Contact-Manager' icon='fas fa-id-card-alt'/>
-            <div className="container">
-                <Switch>
-                    <Route exact path='/' component={Home}/>
-                    <Route exact path='/about' component={About}/>
-                </Switch>
-            </div>
-        </>
+        <ContactState>
+            <>
+                <Navbar title='Contact-Manager' icon='fas fa-id-card-alt'/>
+                <div className="container">
+                    <Switch>
+                        <Route exact path='/' component={Home}/>
+                        <Route exact path='/about' component={About}/>
+                    </Switch>
+                </div>
+            </>
+        </ContactState>
     );
 }
 
---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,62 @@
+import React, {useReducer} from 'react'
+import uuid from 'uuid'
+import ContactContext from "./contactContext";
+
+const ContactState = props => {
+    const initialState = {
+        contacts:
+            [
+                {
+                    id: 2,
+                    name: "Steve",
+                    email: "steve@gmail.com",
+                    phone: "111-111-1111",
+                    type: "professional"
+                },
+                {
+                    id: 2,
+                    name: "Kate",
+                    email: "kate@gmail.com",
+                    phone: "333-333-3333",
+                    type: "personal"
+                },
+                {
+                    id: 3,
+                    type: "personal",
+                    name: "Melissa",
+                    email: "melissa@gmail.com",
+                    phone: "333-444-4444"
+                }
+
+            ]
+
+
+    }
+
+    const [state, dispatch] = useReducer(contact-reducer, initialState);
+
+    //Add Contact
+
+    //Delete Contact
+
+    //Set Current Contact
+
+    //Clear Current Contact
+
+    //Update Contact
+
+    //Filter Contacts
+
+    //Clear Filter
+
+    return (
+        <ContactContext.Provider
+        value={{
+            contacts: state.contacts
+        }}>
+            {props.children}
+        </ContactContext.Provider>
+    )
+}
+
+export default ContactState;

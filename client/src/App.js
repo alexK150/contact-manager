c2045744d59896181c41fd23bce82b495497e8d2
Alex <akuzin20@gmail.com>
2019-10-17 22:59:15 +0300

    init v20 Added Auth Context

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,23 @@ import {Route, Switch} from "react-router-dom";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import ContactState from "./context/contact/ContactState";
+import AuthState from "./context/auth/AuthState";
 
 const App = () => {
     return (
-        <ContactState>
-            <>
-                <Navbar title='Contact-Manager' icon='fas fa-id-card-alt'/>
-                <div className="container">
-                    <Switch>
-                        <Route exact path='/' component={Home}/>
-                        <Route exact path='/about' component={About}/>
-                    </Switch>
-                </div>
-            </>
-        </ContactState>
+        <AuthState>
+            <ContactState>
+                <>
+                    <Navbar title='Contact-Manager' icon='fas fa-id-card-alt'/>
+                    <div className="container">
+                        <Switch>
+                            <Route exact path='/' component={Home}/>
+                            <Route exact path='/about' component={About}/>
+                        </Switch>
+                    </div>
+                </>
+            </ContactState>
+        </AuthState>
     );
 }
 
